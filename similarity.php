218679c9037297d20512cfcc4c914de2e34c2586
Harits Rahman Mazayamusthafa <32640545+rietzche@users.noreply.github.com>
2018-12-26 04:03:40 +0700

    Add files via upload

---
+++ similarity.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Algoritma Cosine</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</head>
+<body class="container">
+	<hr>
+	<center>
+		<h1>Similarity Hadith System.</h1>
+	</center>
+	<hr>
+	<div class="alert alert-success" role="alert">
+	  <h4 class="alert-heading">Berhasil!</h4>
+	  <p>Berikut ini adalah hasil dari perbandingan hadits-hadits yang telah anda inputkan.</p>
+	  <hr>
+	  <p>tingkat kesamaannya adalah 63,2%</p>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<div class="card text-center border-primary">
+			  <div class="card-header">
+			    Hadits pertama
+			  </div>
+			  <div class="card-body">
+			    <p class="card-text">"Siapa yang curang bukan golongan kami"</p>
+			  </div>
+			  <div class="card-footer text-muted">
+			    6 kata, masih kurang 300 kata.
+			  </div>
+			</div>
+		</div>
+
+		<div class="col-md-6">
+			<div class="card text-center border-secondary">
+			  <div class="card-header">
+			    Hadits kedua
+			  </div>
+			  <div class="card-body">
+			    <p class="card-text">"Siapa merampas milik orang lain bukan golongan kami"</p>
+			  </div>
+			  <div class="card-footer text-muted">
+			    8 kata, masih kurang 300 kata.
+			  </div>
+			</div>
+		</div>
+	</div>
+	<hr>
+	<center>
+		<button class="btn btn-info">Cek sekarang!</button>
+	</center>
+</body>
+</html>
